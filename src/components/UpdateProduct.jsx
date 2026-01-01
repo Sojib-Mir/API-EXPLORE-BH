e7cc76f2eada3ebca00338f28c168b8f12d91d4c
SojibMir27 <mdsojibmir2027@gmail.com>
2026-01-01 19:40:08 +0600

    added update some ui

--- src/components/UpdateProduct.jsx
+++ src/components/UpdateProduct.jsx
@@ -5,14 +5,13 @@ const UpdateProduct = ({ isOpen, setIsEditModalOpen, product }) => {
 
   const handleUpdate = (e) => {
     e.preventDefault();
-
     const form = e.target;
-
     const updatedInfo = {
       name: form.name.value,
       price: form.price.value,
       category: form.category.value,
       image: form.image.value,
+      description: form.description.value,
     };
 
     // Backend API call for Update (PATCH)
@@ -23,7 +22,7 @@ const UpdateProduct = ({ isOpen, setIsEditModalOpen, product }) => {
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
+        console.log(data);
         if (data.modifiedCount === 1) {
           Swal.fire("Success", "Product Updated Successfully!", "success");
           setIsEditModalOpen(false);
@@ -47,46 +46,80 @@ const UpdateProduct = ({ isOpen, setIsEditModalOpen, product }) => {
         </h3>
 
         <form onSubmit={handleUpdate} className="space-y-4">
+          {/* name */}
           <div className="form-control">
-            <label className="label">Product Name</label>
+            <label className="label text-white">Product Name</label>
             <input
               name="name"
               type="text"
               defaultValue={product.name}
-              className="input input-bordered"
+              className="mt-1  block w-full px-3 py-2 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+              focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
               required
             />
           </div>
-
+          {/* price */}
           <div className="form-control">
-            <label className="label">Price</label>
+            <label className="label text-white">Price</label>
             <input
               name="price"
               type="number"
               defaultValue={product.price}
-              className="input input-bordered"
+              className="mt-1  block w-full px-3 py-2 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+              focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
               required
             />
           </div>
 
+          {/* Category */}
           <div className="form-control">
-            <label className="label">Category</label>
-            <input
+            <label className="block text-sm font-medium  ">
+              Product Category
+            </label>
+            <select
               name="category"
-              type="text"
-              defaultValue={product.category}
-              className="input input-bordered"
+              className="mt-1 block  w-full px-3 py-2 bg-sky-400/30 text-black border border-gray-300 rounded-md text-sm shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
               required
-            />
+              defaultValue={product.category}
+            >
+              <option value="">Select a Category</option>
+              <option value="Electronics">Electronics</option>
+              <option value="Home Decor">Home Decor</option>
+              <option value="Fashion">Fashion</option>
+              <option value="Internet">Internet</option>
+              <option value="Beauty">Health & Beauty</option>
+              <option value="Sports">Sports & Outdoors</option>
+            </select>
           </div>
 
+          {/* Description */}
           <div className="form-control">
-            <label className="label">Image URL</label>
+            <label className="block text-sm font-medium  ">
+              Product Description
+            </label>
+            <textarea
+              type="text"
+              name="description"
+              className="mt-1  block w-full px-3 py-2 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+              focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 "
+              cols="30"
+              rows="5"
+              placeholder="Product Descripton here"
+              step="0.01"
+               defaultValue={product.description}
+              required
+            ></textarea>
+          </div>
+
+          {/* Image URL */}
+          <div className="form-control">
+            <label className="label text-white">Image URL</label>
             <input
               name="image"
               type="text"
               defaultValue={product.image}
-              className="input input-bordered"
+              className="mt-1  block w-full px-3 py-2 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+              focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
               required
             />
           </div>
